675c7a972becc9d5365216f7e4d860fdb4eb1e0a
Me <me@localhost>
2022-05-27 01:50:29 +0300

    File upload added

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -58,5 +62,41 @@ public class MainController: Controller {
         var result = await dbContext.Shots.ToListAsync();
         return View();
     }
+
+    [HttpGet("upload")]
+    public IActionResult UploadFile() {
+        return View();
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> StoreFile(List<IFormFile> files) {
+
+        long size = files.Sum(f => f.Length);
+
+        var filePaths = new List<string>();
+        foreach (var formFile in files) {
+            System.Console.Write("\n=============\n");
+            System.Console.Write(formFile.Name);
+            System.Console.Write("\n");
+            System.Console.Write(formFile.FileName);
+            System.Console.Write("\n");
+            System.Console.Write(formFile.ContentDisposition);
+            System.Console.Write("\n=============\n");
+            if (formFile.Length > 0) {
+                // full path to file in temp location
+                var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+                filePaths.Add(filePath);
+                using (var stream = new FileStream(filePath + ".TMP", FileMode.Create)) {
+                    System.Console.Write(filePath);
+                    await formFile.CopyToAsync(stream);
+                }
+            }
+        }
+
+        // process uploaded files
+        // Don't rely on or trust the FileName property without validation.
+        return Ok(new { count = files.Count, size, filePaths });
+    }
+
 
 }

4f4bc7c931f2eed977304af32e072f43bab51340
Me <me@localhost>
2022-10-30 23:59:07 +0300

    all works but for single uploaded file

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -115,6 +115,7 @@ public class MainController: Controller {
     [RequestSizeLimit(1000_000_000)]
     [HttpPost("upload_shots")]
     public async Task<IActionResult> StoreFile(List<IFormFile> files, int albumId) {
+        User user = dbContext.Users.Where(u => u.Username == HttpContext.User.Identity.Name).Include(u => u.Storage).First();
         ShotStorage storage = dbContext.ShotStorages.Find(1);
         Album album = await dbContext.Albums.FindAsync(albumId);
         long size = files.Sum(f => f.Length);
@@ -127,9 +128,13 @@ public class MainController: Controller {
 
                 try {
                     using var stream = new MemoryStream();
+                    using var stream1 = new MemoryStream();
                     using var outputStream = new MemoryStream();
                     await formFile.CopyToAsync(stream);
+                    await formFile.CopyToAsync(stream1);
                     stream.Position = 0;
+                    stream1.Position = 0;
+
                     using var image = Image.Load(stream);
                     float ratio = (float)image.Width/(float)image.Height;
                     System.Console.Write("ratio is " + ratio);
@@ -151,13 +156,13 @@ public class MainController: Controller {
                     stream.Position = 0;
 
                     shot.MD5 = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
-
                     dbContext.Shots.Add(shot);
                     await dbContext.SaveChangesAsync();
 
                     shot.SourceUri = "" + shot.ShotId;
                     shot.Storage = storage;
-                    Storage.StoreShot(shot, stream);
+                    Storage.StoreShot(shot, stream1);
+
                     await dbContext.SaveChangesAsync();
                 }   catch (DbUpdateException e) {
                     System.Console.Write("The error is " + e.Data);

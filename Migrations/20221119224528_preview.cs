11cdbf3fbfeab022690ccb513c1af699a2e6e1f7
drevald <ddreval@gmail.com>
2022-11-20 02:16:51 +0300

    Preview added to album

---
+++ Migrations/20221119224528_preview.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace svema.Migrations
+{
+    public partial class preview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PreviewId",
+                table: "Albums",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviewId",
+                table: "Albums");
+        }
+    }
+}

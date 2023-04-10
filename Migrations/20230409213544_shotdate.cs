14511e184d6bdecd60b431abaf99dab37368b36d
Me <me@localhost>
2023-04-10 10:20:43 +0300

    migrations added

---
+++ Migrations/20230409213544_shotdate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace svema.Migrations
+{
+    public partial class shotdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Shots",
+                newName: "DateStart");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateEnd",
+                table: "Shots",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateEnd",
+                table: "Shots");
+
+            migrationBuilder.RenameColumn(
+                name: "DateStart",
+                table: "Shots",
+                newName: "Date");
+        }
+    }
+}

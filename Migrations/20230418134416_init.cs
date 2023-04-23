7537a8a219f4411b59f4fa4585c4b2e3c46f0189
Denis Dreval <dreval@narod.ru>
2023-04-23 14:06:41 +0300

    migration added

---
+++ Migrations/20230418134416_init.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace svema.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "locations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    longitude = table.Column<float>(type: "real", nullable: false),
+                    latitude = table.Column<float>(type: "real", nullable: false),
+                    location_precision_meters = table.Column<int>(type: "integer", nullable: false),
+                    zoom = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_locations", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "persons",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: true),
+                    last_name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_persons", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "storages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    auth_token = table.Column<string>(type: "text", nullable: true),
+                    refresh_token = table.Column<string>(type: "text", nullable: true),
+                    provider = table.Column<string>(type: "text", nullable: true),
+                    root = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_storages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    username = table.Column<string>(type: "text", nullable: true),
+                    password_hash = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    StorageId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_users_storages_StorageId",
+                        column: x => x.StorageId,
+                        principalTable: "storages",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "albums",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: true),
+                    preview_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_albums", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_albums_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "album_comments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    author_id = table.Column<int>(type: "integer", nullable: false),
+                    author_username = table.Column<string>(type: "text", nullable: true),
+                    album_id = table.Column<int>(type: "integer", nullable: false),
+                    time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_album_comments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_album_comments_albums_album_id",
+                        column: x => x.album_id,
+                        principalTable: "albums",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_album_comments_users_author_id",
+                        column: x => x.author_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "shots",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    album_id = table.Column<int>(type: "integer", nullable: false),
+                    date_start = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    date_end = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    preview = table.Column<byte[]>(type: "bytea", nullable: true),
+                    source_uri = table.Column<string>(type: "text", nullable: true),
+                    location_id = table.Column<int>(type: "integer", nullable: true),
+                    md5 = table.Column<string>(type: "text", nullable: true),
+                    content_type = table.Column<string>(type: "text", nullable: true),
+                    StorageId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shots", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_shots_albums_album_id",
+                        column: x => x.album_id,
+                        principalTable: "albums",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_shots_locations_location_id",
+                        column: x => x.location_id,
+                        principalTable: "locations",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_shots_storages_StorageId",
+                        column: x => x.StorageId,
+                        principalTable: "storages",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonShot",
+                columns: table => new
+                {
+                    PersonsPersonId = table.Column<int>(type: "integer", nullable: false),
+                    ShotsShotId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonShot", x => new { x.PersonsPersonId, x.ShotsShotId });
+                    table.ForeignKey(
+                        name: "FK_PersonShot_persons_PersonsPersonId",
+                        column: x => x.PersonsPersonId,
+                        principalTable: "persons",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonShot_shots_ShotsShotId",
+                        column: x => x.ShotsShotId,
+                        principalTable: "shots",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "shot_comments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    author_id = table.Column<int>(type: "integer", nullable: false),
+                    author_username = table.Column<string>(type: "text", nullable: true),
+                    shot_id = table.Column<int>(type: "integer", nullable: false),
+                    time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shot_comments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_shot_comments_shots_shot_id",
+                        column: x => x.shot_id,
+                        principalTable: "shots",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_shot_comments_users_author_id",
+                        column: x => x.author_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_album_comments_album_id",
+                table: "album_comments",
+                column: "album_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_album_comments_author_id",
+                table: "album_comments",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_albums_UserId",
+                table: "albums",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonShot_ShotsShotId",
+                table: "PersonShot",
+                column: "ShotsShotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shot_comments_author_id",
+                table: "shot_comments",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shot_comments_shot_id",
+                table: "shot_comments",
+                column: "shot_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shots_album_id",
+                table: "shots",
+                column: "album_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shots_location_id",
+                table: "shots",
+                column: "location_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shots_md5",
+                table: "shots",
+                column: "md5",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shots_StorageId",
+                table: "shots",
+                column: "StorageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_StorageId",
+                table: "users",
+                column: "StorageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "album_comments");
+
+            migrationBuilder.DropTable(
+                name: "PersonShot");
+
+            migrationBuilder.DropTable(
+                name: "shot_comments");
+
+            migrationBuilder.DropTable(
+                name: "persons");
+
+            migrationBuilder.DropTable(
+                name: "shots");
+
+            migrationBuilder.DropTable(
+                name: "albums");
+
+            migrationBuilder.DropTable(
+                name: "locations");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "storages");
+        }
+    }
+}
---
+++ Migrations/20230418212857_shotsize.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace svema.Migrations
+{
+    public partial class shotsize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "size",
+                table: "shots",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "size",
+                table: "shots");
+        }
+    }
+}
